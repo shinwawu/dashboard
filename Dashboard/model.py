591adc4a63c982b924a307a671fd9ac627cccb57
shinwawu <washingtonying@hotmail.com>
2025-05-08 02:09:05 -0300

    Update model.py

--- Dashboard/model.py
+++ Dashboard/model.py
@@ -11,4 +11,18 @@ def LeituraCPU():
     return tempototal, tempoparado
 
 
-
+def Quantidade_Processos_Threads():
+    processos = 0
+    threads = 0
+    for ProcessosID in os.listdir("/proc"):
+        if ProcessosID.isdigit():
+            processos += 1
+            try:
+                with open(f"/proc/{ProcessosID}/status") as Process:
+                    for linha in Process:
+                        if linha.startswith("Threads:"):
+                            threads += int(linha.split()[1])
+                            break
+            except:
+                continue
+    return processos, threads
